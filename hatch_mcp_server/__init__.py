aeed9eafa63bec6281085f0dab979086282005cf
LittleCoinCoin <eliott.jacopin@laposte.net>
2025-07-06 23:51:48 +0900

    [Add] Packaging metadata
    **Major**:
    - Added the `__init__.py` and the `pyproject.toml` to make this installable by major python package managers

---
+++ hatch_mcp_server/__init__.py
@@ -0,0 +1,17 @@
+"""
+Hatch MCP Server - The wrapper for MCP servers to service Hatch-related applications.
+
+This package provides a class for extending MCP servers with additional functionalities
+relevant to the Hatch ecosystem. This includes:
+
+- Citations for Origin Software and MCP server implementations as MCP resources.
+
+"""
+
+__version__ = "0.1.0"
+
+from .hatch_mcp import HatchMCP
+
+__all__ = [
+    'HatchMCP'
+]
